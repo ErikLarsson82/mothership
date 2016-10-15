63473e73ed9bb263f451562f9170ec000caa649b
Erik Larsson <erik.mattias.larsson@gmail.com>
2016-10-15 13:13:37 +0200

    Full map with different objects

--- game.js
+++ game.js
@@ -7,10 +7,16 @@ define('game', [
     userInput,
     map
 ) {
+    //Refactor:
+    // - Red ut "types" och index. Player med index 1,2,3 osv. Waves ska kunna peka ut SPAWN1 och inte bara ett index
+    // - Classer behöver TYPES som input, helt redundant
+
     // 144 or 60
     const FPS = 144;
 
-    const gameObjects = [];
+    let gameObjects = [];
+    let spawnObjects = [];
+    let waveController = null;
     window.gameObjects = gameObjects;
 
     const GRID_SIZE = 20;
@@ -23,11 +29,62 @@ define('game', [
             this.type = type;
             this.pos = pos;
             this.markedForRemoval = false;
+            this.color = "black";
+        }
+        remove () {
+            return this.markedForRemoval;
         }
         tick() {}
         draw() {
-            context.fillStyle = "green";
+            context.fillStyle = this.color;
             context.fillRect(this.pos.x, this.pos.y, GRID_SIZE, GRID_SIZE);
+        }
+    }
+
+    class Debree extends GameObject {
+        constructor(pos, type) {
+            super(pos, type);
+            this.color = "blue";
+        }
+    }
+
+    class Flyer extends GameObject {
+        constructor(pos, type) {
+            super(pos, type);
+            this.color = "gray";
+        }
+        tick() {
+            var motherPos = findGameObj(map.types.MOTHERSHIP).pos;
+            var x = (motherPos.x > this.pos.x) ? this.pos.x + 0.1 : this.pos.x - 0.1;
+            var y = (motherPos.y > this.pos.y) ? this.pos.y + 0.1 : this.pos.y - 0.1;
+            var newPos = {
+                x: x,
+                y: y
+            }
+            attemptMove(this, newPos);   
+        }
+    }
+
+    class Spawn extends GameObject {
+        constructor(pos, type, id) {
+            super(pos, type);
+            this.id = id;
+            this.color = "red";
+        }
+    }
+
+    class Part extends GameObject {
+        constructor(pos, type) {
+            super(pos, type);
+            this.color = "orange";
+        }
+    }
+
+    class Mothership extends GameObject {
+        constructor(pos, type) {
+            super(pos, type);
+            this.color = "green";
+            this.hp = 100;
         }
     }
 
@@ -35,6 +92,8 @@ define('game', [
         constructor(pos, type, id) {
             super(pos, type);
             this.id = id;
+            this.color = "purple";
+            this.debree = 0;
         }
         tick() {
             var pad = userInput.readInput()[this.id];
@@ -46,21 +105,83 @@ define('game', [
             }
             attemptMove(this, newPos);
         }
-        draw() {
-            context.fillStyle = "red";
-            context.fillRect(this.pos.x, this.pos.y, GRID_SIZE, GRID_SIZE);
+    }
+
+    class WaveController {
+        constructor(waves) {
+            this.waves = waves || [];
+            this.currentWave = null;
+        }
+        tick() {
+            if (this.currentWave) {
+                this.currentWave.counter++;
+                if (this.currentWave.counter >= this.currentWave.duration) this.currentWave = null;
+            } else {
+                var blueprint = this.waves.shift() || {};
+                if (blueprint.duration) {
+                    this.currentWave = {
+                        duration: blueprint.duration * FPS,
+                        counter: 0
+                    }
+                } else if (blueprint.type) {
+                    switch(blueprint.type) {
+                        case map.types.FLYER:
+                            var targetPos = _.clone(findGameObjWithIndex(map.types.SPAWN, blueprint.spawnIdx).pos);
+                            var motherPos = findGameObj(map.types.MOTHERSHIP).pos;
+                            targetPos.x = (motherPos.x > targetPos.x) ? targetPos.x + GRID_SIZE : targetPos.x - GRID_SIZE;
+                            targetPos.y = (motherPos.y > targetPos.y) ? targetPos.y + GRID_SIZE : targetPos.y - GRID_SIZE;
+                            gameObjects.push(new Flyer(targetPos, map.types.FLYER));
+                        break;
+                    }
+                }
+            }
         }
     }
 
+    function findGameObj(type) {
+        return _.find(gameObjects, function(item) {
+            return item.type === type;
+        });
+    }
+    function findGameObjWithIndex(type, idx) {
+        return _.find(gameObjects, function(item) {
+            return item.type === type && item.id === idx;
+        })
+    }
+    window.findGameObj = findGameObj;
+    window.findGameObjWithIndex = findGameObjWithIndex;
+
+    function typeCheck(obj1, obj2, type1, type2) {
+        return (obj1.type === type1 && obj2.type === type2 || obj2.type === type1 && obj1.type === type2)
+    }
+
     function collision(obj1, obj2) {
-        if (obj1.type === map.types.PLAYER1 && obj2.type === map.types.PLAYER2 || obj2.type === map.types.PLAYER1 && obj1.type === map.types.PLAYER2) {
-            // players are colliding'
+
+        if (typeCheck(obj1, obj2, map.types.PLAYER, map.types.FLYER)) {
+            var player = (obj1.type === map.types.PLAYER) ? obj1 : obj2;
+            var flyer = (obj1.type === map.types.FLYER) ? obj1 : obj2;
+            if (player.id === 0) {
+                flyer.markedForRemoval = true;
+            } else {
+                player.disabled = true;
+            }
+        }
+        if (typeCheck(obj1, obj2, map.types.FLYER, map.types.MOTHERSHIP)) {
+            var mothership = (obj1.type === map.types.MOTHERSHIP) ? obj1 : obj2;
+            mothership.hp = mothership.hp - 0.01;
+        }
+        if (typeCheck(obj1, obj2, map.types.PLAYER, map.types.DEBREE)) {
+            var player = (obj1.type === map.types.PLAYER) ? obj1 : obj2;
+            var debree = (obj1.type === map.types.DEBREE) ? obj1 : obj2;
+            player.debree = player.debree + 1;
+            debree.markedForRemoval = true;
         }
     }
 
     function attemptMove(gameObject, newPos) {
         var resultX = _.find(gameObjects, function(obj) {
             if (gameObject === obj) return false;
+            if (!obj.pos) debugger;
             var legal = (
                 (newPos.x > obj.pos.x + GRID_SIZE) || (newPos.x + GRID_SIZE < obj.pos.x) ||
                 (gameObject.pos.y > obj.pos.y + GRID_SIZE) || (gameObject.pos.y + GRID_SIZE < obj.pos.y)
@@ -89,32 +210,103 @@ define('game', [
                         gameObjects.push(new GameObject({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.WALL));
                     break;
                     case map.types.PLAYER1:
-                        gameObjects.push(new Player({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.PLAYER1, 0));
+                        gameObjects.push(new Player({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.PLAYER, 0));
                     break;
                     case map.types.PLAYER2:
-                        gameObjects.push(new Player({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.PLAYER2, 1));
+                        gameObjects.push(new Player({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.PLAYER, 1));
+                    break;
+                    case map.types.PLAYER3:
+                        gameObjects.push(new Player({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.PLAYER, 2));
+                    break;
+                    case map.types.DEBREE:
+                        gameObjects.push(new Debree({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.DEBREE));
+                    break;
+                    case map.types.MOTHERSHIP:
+                        gameObjects.push(new Mothership({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.MOTHERSHIP));
+                    break;
+                    case map.types.SPAWN1:
+                        gameObjects.push(new Spawn({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.SPAWN, 0));
+                    break;
+                    case map.types.SPAWN2:
+                        gameObjects.push(new Spawn({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.SPAWN, 1));
+                    break;
+                    case map.types.SPAWN3:
+                        gameObjects.push(new Spawn({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.SPAWN, 2));
+                    break;
+                    case map.types.PART:
+                        gameObjects.push(new Part({x: colIdx * GRID_SIZE, y: rowIdx * GRID_SIZE}, map.types.PART));
                     break;
                 }   
             });
         });
     }
 
+    function endConditions() {
+        //waveController is done and no more enemies -> Map complete! -> 1
+        //all players on the ground -> Game over -> 2
+        //mothership destroyed -> Game over -> 2
+        //else -> false
+        if (waveController.waves.length === 0 && false) {
+            return 1;
+        }
+        if (findGameObj(map.types.MOTHERSHIP).hp <= 0) {
+            return 2;
+        }
+        return false;
+    }
+
     return {
         init: function() {
             generateMap();
+            waveController = new WaveController(map.waves);
+
+            context.font="20px Verdana";
         },
         tick: function() {
+
+            var end = endConditions();
+            if (end) {
+                if (end === 1) {
+                    context.fillStyle = "green"
+                    context.fillRect(100, 100, 400, 100);
+                    context.fillStyle = "black"
+                    context.fillText('Map finished!',130,130);
+                    return;
+                } else if (end === 2) {
+                    context.fillStyle = "red"
+                    context.fillRect(100, 100, 400, 100);
+                    context.fillStyle = "black"
+                    context.fillText('GAME OVER',130,130);
+                    return;
+                }
+            }
+
+            waveController.tick();
 
             _.each(gameObjects, function(gameObject) {
                 gameObject.tick();
             });
 
-            context.fillStyle = "gray"
+            gameObjects = _.filter(gameObjects, function(gameObject) {
+                return !gameObject.remove();
+            });
+
+            _.each(spawnObjects, function(spawnFunction) {
+                gameObjects.push(spawnFunction());
+            });
+            spawnObjects.length = 0;
+
+            context.fillStyle = "#d0d0d0"
             context.fillRect(0, 0, 1024, 768)
 
             _.each(gameObjects, function(gameObject) {
                 gameObject.draw();
             });
+
+            //GUI
+            context.fillStyle = "white"
+            var mothership = findGameObj(map.types.MOTHERSHIP);
+            context.fillText('Mothership HP: ' + mothership.hp.toFixed(0),400,16);
         }
     }
 });
